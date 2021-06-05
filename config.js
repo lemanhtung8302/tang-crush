b84b801f0f289a453031368549d4fc4dfffa3252
lemanhtung8302 <85400253+lemanhtung8302@users.noreply.github.com>
2021-06-05 20:40:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Anh cũng vậy anh yêu bồ nhiều lắm ó',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. https://www.facebook.com/lemanhtung8302
 }
